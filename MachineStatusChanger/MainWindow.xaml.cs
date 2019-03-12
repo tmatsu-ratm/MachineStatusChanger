d309e7897ba490f0f7d7f85c6fdaab7a38c0c1c6
Taro Matsumoto <tmatsu.tfg@gmail.com>
2019-03-12 16:14:11 +0900

    プロジェクト ファイルを追加します。

---
+++ MachineStatusChanger/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MachineStatusChanger
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitEvents()
+        {
+            foreach (var ctrl in LogicalTreeHelper.GetChildren(rootGrid))
+            {
+                if (!(ctrl is Button))
+                {
+                    continue;
+                }
+
+                (ctrl as Button).Click += (sender, e) =>
+                {                    
+                }
+            }
+        }
+    }
+
+}
