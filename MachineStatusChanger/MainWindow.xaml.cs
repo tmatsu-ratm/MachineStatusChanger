7a1b8c7ac9fbbf2811ec58b6c628d87e5ed89d2c
Taro Matsumoto <tmatsu.tfg@gmail.com>
2019-03-14 09:26:17 +0900

    状態変更機能追加

--- MachineStatusChanger/MainWindow.xaml.cs
+++ MachineStatusChanger/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Odbc;
 using System.Linq;
 using System.Text;
@@ -74,28 +75,106 @@ namespace MachineStatusChanger
         private void ClearMachineName()
         {
             MachineNameLabel.Content = "";
+            ChangeStateButton(false);
         }
 
         private void Dbaccess(string machineNo)
         {
-            OdbcConnection con = new OdbcConnection("Dsn=TestDataSource;id=sa;Pwd=3141592;DataBase=eqstatdb;");
-            con.Open();
-            try
+            OdbcDbIf db = new OdbcDbIf();
+            DataTable tb;
+            db.Connect("TestDataSource", "eqstatdb", "sa", "3141592", -1);
+            string q = "select MACHINE_NAME,MACHINE_STR from Status where MACHINE_NO='" + machineNo + "'";
+            tb = db.ExecuteSql(q, -1);
+            if (tb.Rows.Count > 0)
             {
-                string q = "select MACHINE_NAME from Status where MACHINE_NO='" + machineNo + "'";
-                OdbcCommand oc = new OdbcCommand(q, con);
-                OdbcDataReader odr = oc.ExecuteReader();
+                MachineNameLabel.Content = tb.Rows[0]["MACHINE_NAME"].ToString().Trim();
+                int state = (int)(tb.Rows[0]["MACHINE_STR"]);
+                ChangeStateButton(true, state);
+            }
+            else
+            {
                 MachineNameLabel.Content = "Machine No.異常";
-                while (odr.Read() == true)
-                {
-                    MachineNameLabel.Content = ((string) odr[0]).Trim();
-                }
-                odr.Close();
             }
-            finally
+
+            db.Disconnect();
+
+        }
+
+        private void UpdateState(string machineNo, int state)
+        {
+            OdbcDbIf db = new OdbcDbIf();
+            db.Connect("TestDataSource", "eqstatdb", "sa", "3141592", -1);
+            db.BeginTransaction();
+            string q = "update Status set MACHINE_STR=" + state + " where MACHINE_NO='" + machineNo + "'";
+            db.ExecuteSql(q, -1);
+            db.CommitTransaction();
+            db.Disconnect();
+
+            Dbaccess(machineNo);
+        }
+        
+        private void ChangeStateButton(bool isEnable, int currentState = 0)
+        {
+            S1Button.IsEnabled = isEnable;
+            S2Button.IsEnabled = isEnable;
+            S3Button.IsEnabled = isEnable;
+            S4Button.IsEnabled = isEnable;
+            S5Button.IsEnabled = isEnable;
+            S6Button.IsEnabled = isEnable;
+
+            switch (currentState)
             {
-                con.Close();
+                case 1:
+                    S1Button.IsEnabled = false;
+                    break;
+                case 2:
+                    S2Button.IsEnabled = false;
+                    break;
+                case 3:
+                    S3Button.IsEnabled = false;
+                    break;
+                case 4:
+                    S4Button.IsEnabled = false;
+                    break;
+                case 5:
+                    S5Button.IsEnabled = false;
+                    break;
+                case 6:
+                    S6Button.IsEnabled = false;
+                    break;
+                default:
+                    break;
             }
+        }
+
+        private void S1Button_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateState(MachineNoLabel.Content.ToString(), 1);
+        }
+
+        private void S2Button_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateState(MachineNoLabel.Content.ToString(), 2);
+        }
+
+        private void S3Button_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateState(MachineNoLabel.Content.ToString(), 3);
+        }
+
+        private void S4Button_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateState(MachineNoLabel.Content.ToString(), 4);
+        }
+
+        private void S5Button_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateState(MachineNoLabel.Content.ToString(), 5);
+        }
+
+        private void S6Button_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateState(MachineNoLabel.Content.ToString(), 6);
         }
     }
 }
---
+++ MachineStatusChanger/OdbcDbIf.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineStatusChanger
+{
+    class OdbcDbIf
+    {
+        private OdbcConnection _con = null;
+
+        private OdbcTransaction _trn = null;
+
+        public void Connect(String dsn, String dbn, String uid, String pas, int tot)
+        {
+            try
+            {
+                if (_con == null)
+                {
+                    _con = new OdbcConnection();
+                }
+
+                String cst = "";
+                cst = cst + ";DSN=" + dsn;
+                cst = cst + ";Database=" + dbn;
+                cst = cst + ";UID=" + uid;
+                cst = cst + ";PWD=" + pas;
+                if (tot > -1)
+                {
+                    cst = cst + ";Connect Timeout=" + tot.ToString();
+                }
+
+                _con.ConnectionString = cst;
+                _con.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Connect Error", ex);
+            }
+        }
+
+        public void Disconnect()
+        {
+            try
+            {
+                _con.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Disconnect Error", ex);
+            }
+        }
+
+        public DataTable ExecuteSql(String sql, int tot)
+        {
+            DataTable dt = new DataTable();
+
+            try
+            {
+                OdbcCommand sqlCommand = new OdbcCommand(sql, _con, _trn);
+
+                if (tot > -1)
+                {
+                    sqlCommand.CommandTimeout = tot;
+                }
+
+                OdbcDataAdapter adapter = new OdbcDataAdapter(sqlCommand);
+
+                adapter.Fill(dt);
+                adapter.Dispose();
+                sqlCommand.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ExecuteSql Error", ex);
+            }
+
+            return dt;
+        }
+
+        public void BeginTransaction()
+        {
+            try
+            {
+                _trn = _con.BeginTransaction();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("BeginTransaction Error", ex);
+            }
+        }
+
+        public void CommitTransaction()
+        {
+            try
+            {
+                if (_trn != null)
+                {
+                    _trn.Commit();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("CommitTransaction Error", ex);
+            }
+            finally
+            {
+                _trn = null;
+            }
+        }
+
+        public void RollbackTransaction()
+        {
+            try
+            {
+                if (_trn != null)
+                {
+                    _trn.Rollback();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("RollbackTransaction Error", ex);
+            }
+            finally
+            {
+                _trn = null;
+            }
+        }
+
+        ~OdbcDbIf()
+        {
+            Disconnect();
+        }
+    }
+}
